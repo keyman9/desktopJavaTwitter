97f01e1299ef22a717898870e052bf583b2e4774
keyman9 <laxscott@gmail.com>
2015-11-12 10:32:37 -0500

    Did basic formatting. The code in here looks like it should be for the actual createAccount method, though

--- Account.java
+++ Account.java
@@ -9,7 +9,7 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.io.*;
 import java.awt.*;
- import javax.swing.*;
+import javax.swing.*;
 import java.awt.event.*;
 import java.sql.*;
 public class Account {
@@ -19,34 +19,29 @@ public class Account {
  public static void main(String args[]){
 
  
-    JLabel       username = new JLabel("Username: ");
-        JLabel       password = new JLabel("Password : ");
+    JLabel username = new JLabel("Username: ");
+    JLabel password = new JLabel("Password : ");
 
-     JButton register= new JButton("Register: ");
-      JFrame frame = new JFrame();
-      final int FIELD_WIDTH = 20;
-      JTextField textField = new JTextField(FIELD_WIDTH);
+    JButton register = new JButton("Register: ");
+    JFrame frame = new JFrame();
+    final int FIELD_WIDTH = 20;
+    JTextField textField = new JTextField(FIELD_WIDTH);
       
-            JTextField textField2 = new JTextField(FIELD_WIDTH);
+    JTextField textField2 = new JTextField(FIELD_WIDTH);
 
-     frame.setLayout(new FlowLayout());
+    frame.setLayout(new FlowLayout());
+    frame.add(username);
+    frame.add(textField);
+    frame.add(password);
+    frame.add(textField2);
+    frame.add(register);
 
-frame.add(username);
-                frame.add(textField);
-
-frame.add(password);
-                                frame.add(textField2);
-
-     frame.add(register);
-
-     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     
     frame.setVisible(true);
 
-String s1=textField.getText();
+    String s1 = textField.getText();
 
-  
-    
     }
     }
