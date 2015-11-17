aefe2d31eb3854e5c7ad3210977d258428e704cf
keyman9 <laxscott@gmail.com>
2015-11-17 12:19:12 -0500

    Extremely rough idea of main program

---
+++ Twittinator.java
@@ -0,0 +1,24 @@
+package faketwitter;
+
+/**
+ *
+ * @author spencer
+ */
+
+public class Twittinator {
+    public static void main(String[] args){
+        //read in files
+
+        //display public feed
+        
+        //begin main loop
+        
+            //give user choices 
+                //perform choice
+        
+        //end loop
+
+        //write to files   
+    }
+}
+
