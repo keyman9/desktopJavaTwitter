afb84af0a821ee6933d45ade5072f5d2e481d73e
keyman9 <laxscott@gmail.com>
2015-11-17 15:48:16 -0500

    Added menu choices

--- Twittinator.java
+++ Twittinator.java
@@ -15,7 +15,21 @@ public class Twittinator {
         
             //give user choices 
                 //perform choice
-        
+                    //log in 
+                    //if logged in
+                        //display feed
+                        //create message
+                            //public
+                            //subscriber only
+                            //direct message
+                        //edit bio
+                        //subscribe to user
+                        //block user
+                            //display blocked list*
+                        //view profile
+                    //register        
+                    //search for user
+                    //search by keyword
         //end loop
 
         //write to files   
