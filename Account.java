da22ed33ae12e3b3d0ceb355dc07228f2a2ea2c7
gphalen <kpvb@cox.net>
2015-11-17 23:48:03 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -9,39 +9,95 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.io.*;
 import java.awt.*;
-import javax.swing.*;
+ import javax.swing.*;
 import java.awt.event.*;
 import java.sql.*;
 public class Account {
 
 
 
+public void actionPerformed(ActionEvent e){
+
+}
+
  public static void main(String args[]){
 
  
-    JLabel username = new JLabel("Username: ");
-    JLabel password = new JLabel("Password : ");
+    JLabel       username = new JLabel("Username: ");
+        JLabel       password = new JLabel("Password : ");
 
-    JButton register = new JButton("Register: ");
-    JFrame frame = new JFrame();
-    final int FIELD_WIDTH = 20;
-    JTextField textField = new JTextField(FIELD_WIDTH);
+     JButton register= new JButton("Register: ");
+      JFrame frame = new JFrame();
+      final int FIELD_WIDTH = 20;
+      final JTextField textField = new JTextField(FIELD_WIDTH);
       
-    JTextField textField2 = new JTextField(FIELD_WIDTH);
+            JTextField textField2 = new JTextField(FIELD_WIDTH);
+            JTextField textField3 = new JTextField(FIELD_WIDTH);
+            
 
-    frame.setLayout(new FlowLayout());
-    frame.add(username);
-    frame.add(textField);
-    frame.add(password);
-    frame.add(textField2);
-    frame.add(register);
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setLayout(new FlowLayout());
+
+frame.add(username);
+                frame.add(textField);
+
+frame.add(password);
+
+try{
+   String fileName = "inventory.txt";
+      FileReader file = new FileReader(fileName);
+
+                Scanner scan = new Scanner(file);
+                                frame.add(textField2);
+                                 FileReader file1 = new FileReader(fileName);
+
+                Scanner scan1 = new Scanner(file);
+                                final String get=textField.getText();
+                                final JLabel get2= new JLabel();
+                                get2.setText(textField.getText());
+
+                                
+   register.addActionListener(new
+         ActionListener()
+         {
+                    ArrayList<String> Users=new ArrayList<String>();
+
+            public void actionPerformed(ActionEvent event)
+           {
+       if (!Users.contains(textField.getText())){
+       Users.add(textField.getText());
+       }
+      else
+      System.out.print("Username already exists.");
+          for (int y=0;y<Users.size();y++){
+               
+               System.out.print(Users.get(y));
+               
+               
+               
+               
+               }
+            }
+        });     frame.add(register);
+
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     
     frame.setVisible(true);
 
-    String s1 = textField.getText();
+String s1=textField.getText();
 
+  String thing=textField.getText();
+  String thing2= new String(thing);
+  System.out.print(thing2);
+    }
+    
+    catch(FileNotFoundException ex) {
+            System.out.print("File Not Found Exception");                
+        }
+        catch(IOException ex) {  
+            System.out.print("IOException");   
+            
+        }
     }
     }
